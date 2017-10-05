210179642cb24bbb2b33fe02c91b503362365b00
kirkbridge <bkirkbridetaylor@gmail.com>
2017-10-05 17:56:06 -0500

    Finished the challenge. I hid the chunky bacon :(

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,15 @@
 class Product < ApplicationRecord
   validates :name, :price, :stock_quantity, presence: true
 
+  def uniqueProducts
+    @count = Product.all.count
+  end
+
+  def averagePrice
+    @average = ((Product.average(:price)).to_f).round(2)
+  end
+
+  def countInventory
+    @total_stock = Product.sum(:stock_quantity)
+  end
 end
